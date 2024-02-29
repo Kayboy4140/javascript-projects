dc862bd50140f5de8a89ba08e50852f90d733727
Oluwasegun <kto32@drexel.edu>
2024-02-29 12:59:03 -0500

    dom-and-events exercise completed

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,8 +3,24 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+    });
+
+    missionAbort.addEventListener('mouseover', event1 => {
+        missionAbort.style.backgroundColor = 'red';
+    });
+
+    missionAbort.addEventListener('mouseout', event2 => {
+        missionAbort.style.backgroundColor = '';
+    });
+
+    missionAbort.addEventListener('click', event3 => {
+        let response = prompt("Are you ready to abort the mission? Yes/No" );
+        if (response === "Yes") {
+            paragraph.innerHTML = 'Mission aborted! Space shuttle returning home';
+        }    
+    });
 }
 
 window.addEventListener("load", init);
