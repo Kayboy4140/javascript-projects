551674f6f6a8b9564321c3772a954c7291d31ca5
Oluwasegun <kto32@drexel.edu>
2024-03-06 22:57:55 -0500

    fetch exercise completed

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,88 @@
 //TODO: Add Your Code Below
+// let jsonArray = [
+//     {
+//         "id": 1,
+//         "active": false,
+//         "firstName": "Mae",
+//         "lastName": "Jemison",
+//         "skills": [
+//             "Physician", "Chemical Engineer"
+//         ],
+//         "hoursInSpace": 190,
+//         "picture": "mae-jemison.jpg"
+//     },
+//     {
+//         "id": 2,
+//         "active": false,
+//         "firstName": "Frederick",
+//         "lastName": "Gregory",
+//         "skills": [
+//             "Information Systems", "Shuttle Pilot", "Fighter Pilot", "Helicopter Pilot", "Colonel USAF"
+//         ],
+//         "hoursInSpace": 455,
+//         "picture": "frederick-gregory.jpg"
+//     },
+//     {
+//         "id": 3,
+//         "active": false,
+//         "firstName": "Ellen",
+//         "lastName": "Ochoa",
+//         "skills": [
+//             "Physics", "Electrical Engineer"
+//         ],
+//         "hoursInSpace": 979,
+//         "picture": "ellen-ochoa.jpg"
+//     },
+//     {
+//         "id": 4,
+//         "active": false,
+//         "firstName": "Guion",
+//         "lastName": "Bluford",
+//         "skills": [
+//             "Aerospace Engineer", "Philosophy", "Physics", "Colonel USAF",
+//             "Fighter Pilot"
+//         ],
+//         "hoursInSpace": 686,
+//         "picture": "guion-bluford.jpg"
+//     },
+//     {
+//         "id": 5,
+//         "active": false,
+//         "firstName": "Sally",
+//         "lastName": "Ride",
+//         "skills": [
+//             "Physicist", "Astrophysics"
+//         ],
+//         "hoursInSpace": 343,
+//         "picture": "sally-ride.jpg"
+//     },
+//     {
+//         "id": 6,
+//         "active": true,
+//         "firstName": "Kjell",
+//         "lastName": "Lindgren",
+//         "skills": [
+//             "Physician", "Surgeon", "Emergency Medicine"
+//         ],
+//         "hoursInSpace": 15,
+//         "picture": "kjell-lindgren.jpg"
+//     },
+//     {
+//         "id": 7,
+//         "active": true,
+//         "firstName": "Jeanette",
+//         "lastName": "Epps",
+//         "skills": [
+//             "Physicist", "Philosophy", "Aerospace Engineer"
+//         ],
+//         "hoursInSpace": 0,
+//         "picture": "jeanette-epps.jpg"
+//     }
+// ];
+
+// for (let i = 0; i < jsonArray.length; i++) {
+//     let name = jsonArray[i].firstName + jsonArray[i].lastName;
+//     let hours = jsonArray[i].hoursInSpace;
+//     let status = jsonArray[i].active;
+//     let skill = jsonArray[i].skills.join();
+// }
